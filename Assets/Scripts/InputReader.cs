45385bdb6f8c70dbf2c44d6a6de35ae41bef5c25
huyhoangnguyenakatiger <huyhoangnguyenakatiger@gmail.com>
2024-12-20 21:51:52 +0700

    add InputReader

---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Match3
+{
+    public class InputReader : MonoBehaviour
+    {
+        PlayerInput playerInput;
+        InputAction selectAction;
+        InputAction fireAction;
+        public event Action Fire;
+
+        public Vector2 Selected => selectAction.ReadValue<Vector2>();
+        void Start()
+        {
+            playerInput = GetComponent<PlayerInput>();
+            selectAction = playerInput.actions["Select"];
+            fireAction = playerInput.actions["Fire"];
+            fireAction.performed += OnFire;
+        }
+
+        void OnDestroy()
+        {
+            fireAction.performed -= OnFire;
+        }
+        void OnFire(InputAction.CallbackContext obj) => Fire?.Invoke();
+    }
+}
